f2ab4261f2f7992f382bfd7be06f92d662251284
chsmorg <58636766+chsmorg@users.noreply.github.com>
2022-01-20 20:36:24 -0600

    Initial Commit

---
+++ Message/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Message
+//
+//  Created by chase morgan on 1/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Message/MessageApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessageApp.swift
+//  Message
+//
+//  Created by chase morgan on 1/20/22.
+//
+
+import SwiftUI
+
+@main
+struct MessageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
